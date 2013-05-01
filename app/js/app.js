27fc34856ad9b924ee9b9dfb9c9d82d9776ff648
David Mosher <davidmosher@gmail.com>
2013-04-30 22:01:18 -0600

    Step 3 - added a second route, configuration, and template - refactored our app code so we didn't have to keep writing "angular.module" all over the place

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,25 @@
-angular.module("app", []).config(function($routeProvider) {
+var app = angular.module("app", [])
+
+app.config(function($routeProvider) {
 
   $routeProvider.when('/login', {
     templateUrl: 'login.html',
     controller: 'LoginController'
   });
 
+  $routeProvider.when('/home', {
+    templateUrl: 'home.html',
+    controller: 'HomeController'
+  });
+
   $routeProvider.otherwise({ redirectTo: '/login' });
 
 });
 
-angular.module("app").controller("LoginController", function($scope) {
+app.controller("LoginController", function($scope) {
+
+});
+
+app.controller("HomeController", function($scope) {
 
 });
