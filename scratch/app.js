2daa9f2bdb06494cdd00802373a7e2fbb15392f2
David Mosher <davidmosher@gmail.com>
2013-06-13 15:26:28 -0600

    A place to start when doing this demo

---
+++ scratch/app.js
@@ -0,0 +1,66 @@
+var app = angular.module("app", [])
+
+app.config(function($routeProvider) {
+
+  $routeProvider.when('/login', {
+    templateUrl: 'login.html',
+    controller: 'LoginController'
+  });
+
+  $routeProvider.when('/home', {
+    templateUrl: 'home.html',
+    controller: 'HomeController'
+  });
+
+  $routeProvider.otherwise({ redirectTo: '/login' });
+
+});
+
+app.factory("AuthenticationService", function($location) {
+  return {
+    login: function(credentials) {
+      if (credentials.username !== "ralph" || credentials.password !== "wiggum") {
+        alert("Username must be 'ralph', password must be 'wiggum'");
+      } else {
+        $location.path('/home');
+      }
+    },
+    logout: function() {
+      $location.path('/login');
+    }
+  };
+});
+
+app.controller("LoginController", function($scope, $location, AuthenticationService) {
+  $scope.credentials = { username: "", password: "" };
+
+  $scope.login = function() {
+    AuthenticationService.login($scope.credentials);
+  }
+});
+
+app.controller("HomeController", function($scope, AuthenticationService) {
+  $scope.title = "Awesome Home";
+  $scope.message = "Mouse Over these images to see a directive at work!";
+
+  $scope.logout = function() {
+    AuthenticationService.logout();
+  };
+});
+
+app.directive("showsMessageWhenHovered", function() {
+  return {
+    restrict: "A", // A = Attribute, C = CSS Class, E = HTML Element, M = HTML Comment
+    link: function(scope, element, attributes) {
+      var originalMessage = scope.message;
+      element.bind("mouseenter", function() {
+        scope.message = attributes.message;
+        scope.$apply();
+      });
+      element.bind("mouseleave", function() {
+        scope.message = originalMessage;
+        scope.$apply();
+      });
+    }
+  };
+});
